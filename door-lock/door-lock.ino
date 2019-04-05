8fe4f69c97ddcbd876bf3cd0b840fdc5005ca7f8
Jeremy Jonas <42256007+Jeremy-Jonas@users.noreply.github.com>
2019-04-05 16:13:47 +1300

    Added long press function & made code easier to read

--- door-lock/door-lock.ino
+++ door-lock/door-lock.ino
@@ -1,54 +1,79 @@
 #include <SPI.h>
 #include <MFRC522.h>
- 
+
 #define SS_PIN 10
 #define RST_PIN 9
+
+//Define some keywords (purely to make the code more readable to humans)
+#define pressed LOW
+#define notPressed HIGH
+#define on HIGH
+#define off LOW
+
+//define the inputs
+#define callButton digitalRead(8)
+#define internalButton digitalRead(6)
+
+//define the outputs
+#define doorStrike 2
+#define notificationLED 4
+#define resetRelay 5
+#define unauthorisedLED 7
+
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
- 
-void setup() 
+
+unsigned int pressTime = 0;   //create an integer variable to be incremented while the button is pressed
+
+void setup()
 {
   Serial.begin(9600);   // Initiate a serial communication
   SPI.begin();      // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
   Serial.println("Approximate your card to the reader...");
   Serial.println();
-  pinMode(2, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(8, INPUT_PULLUP);
+  pinMode(doorStrike, OUTPUT);
+  pinMode(unauthorisedLED, OUTPUT);
+  pinMode(notificationLED, OUTPUT);
+  pinMode(8, INPUT_PULLUP); //have to use the pin numbers when initalising inputs due to using "digitalRead(x)" in the definitions
   pinMode(6, INPUT_PULLUP);
 
 }
 void loop() {
-  int pin8 = digitalRead(8);
-  if (pin8 == LOW) {
-    digitalWrite(4, HIGH);
+  if (callButton == pressed && pressTime < 150) { //when the call button is pressed start incrementing pressTime until either the button is released or 15 seconds elapses
+    pressTime++;
+    delay(100); //a delay of 100 means it checks the status of the button every tenth of a second, and means a value of 150 in pressTime = 15 seconds
+  } else if (callButton == pressed && pressTime >= 150) { //if the call button has been pressed and held for 15 seconds turn on the reset relay
+    digitalWrite(resetRelay, on);
+    //program will end here in this case as the arduino resets.
+  } else if (callButton == notPressed && pressTime != 0) { //if the call button is released before 15 seconds, turn on the internal "call notification" LED
+    digitalWrite(notificationLED, on);
+    pressTime = 0;
+    delay(100);
   }
 
-  int pin6 = digitalRead(6);
-  if (pin6 == LOW) {
-    digitalWrite(4, LOW);
+  if (internalButton == pressed) {
+    digitalWrite(notificationLED, off);
   }
   // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  if ( ! mfrc522.PICC_IsNewCardPresent())
   {
     return;
   }
   // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  if ( ! mfrc522.PICC_ReadCardSerial())
   {
     return;
   }
   //Show UID on serial monitor
   Serial.print("UID tag :");
-  String content= "";
+  String content = "";
   byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  for (byte i = 0; i < mfrc522.uid.size; i++)
   {
-     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-     Serial.print(mfrc522.uid.uidByte[i], HEX);
-     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
   Serial.println();
   Serial.print("Message : ");
@@ -57,23 +82,23 @@ void loop() {
   {
     Serial.println("Authorised access"); //send "authorised access" message to the serial monitor (if arduino is connected to PC)
     Serial.println();
-    digitalWrite(2, HIGH); //activate the relay which drives the electronic doorstrike
+    digitalWrite(doorStrike, on); //activate the relay which drives the electronic doorstrike
     delay(3000);
-    digitalWrite(2, LOW); //deactivate the relay which drives the electronic doorstrike
+    digitalWrite(doorStrike, off); //deactivate the relay which drives the electronic doorstrike
   }
 
   else if (content.substring(1) == "** ** ** **") //add a second UID here if you want to use more than one card
   {
     Serial.println("Authorised access");
     Serial.println();
-    digitalWrite(2, HIGH);
+    digitalWrite(doorStrike, on);
     delay(3000);
-    digitalWrite(2, LOW);
+    digitalWrite(doorStrike, off);
   }
- 
+
   else   {
     Serial.println("Access denied");
-    digitalWrite(7, HIGH); //illuminate led on pin 7 to indicate an access attempt was made with unauthorised card      
+    digitalWrite(unauthorisedLED, on); //illuminate led on pin 7 to indicate an access attempt was made with unauthorised card
   }
-  
-} 
+
+}
